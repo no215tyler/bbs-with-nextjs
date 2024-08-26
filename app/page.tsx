02b618260bfd481e7ec69099c0e58b6d876a2774
no215tyler <no.215.tyler@gmail.com>
2024-08-26 23:15:11 +0900

    🔧 prismaのインスタンス化

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import BBSCardList from "./components/BBSCardList";
+import prisma from "../lib/prismaClient";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/post", {
+    cache: "no-store",
+  });
   return (
     <main>
       <BBSCardList />
