796a0cc3d8a218d50425e7bc29f30e74765c9969
no215tyler <no.215.tyler@gmail.com>
2024-08-27 11:31:01 +0900

    ✨ 掲示板新規投稿画面の実装(フロントのみ)

---
+++ app/bbs-posts/create/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "ユーザー名は2文字以上で入力してください。" }),
+  title: z
+    .string()
+    .min(2, { message: "タイトルは2文字以上で入力してください。" }),
+  content: z
+    .string()
+    .min(10, { message: "本文は10文字以上で入力してください。" })
+    .max(140, { message: "本文は140文字以内で入力してください。" }),
+});
+
+const CreateBBSPage = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  const onSubmit = async () => {};
+
+  return (
+    <div className="bg-slate-50 py-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-3 w-1/2 px-7"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ユーザー名</FormLabel>
+                <FormControl>
+                  <Input placeholder="ユーザー名" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>タイトル</FormLabel>
+                <FormControl>
+                  <Input placeholder="タイトル" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>本文</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="投稿内容"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateBBSPage;
